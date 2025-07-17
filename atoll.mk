519c3165de2b222fdce8f7a642b6985d89725bea
rickastlee <45075899+rickastlee@users.noreply.github.com>
2025-07-17 21:17:04 +0200

    Revert "sm6250-common: Build LiveDisplay HIDL"
    This reverts commit 346597a7730e3e2788a906c7a4dfcc7a7475be6b.

--- atoll.mk
+++ atoll.mk
@@ -195,10 +195,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_atoll
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
